6ff6d90ccb3ab56126e9e321d9d91a17a101100b
iFangg <i.Fangg@proton.me>
2025-12-01 13:06:28 +1100

    Feat: Implemented writing to csvs and updated README

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # finance-tracker
 
-Use this finance tracker to log transactions on spreadsheets
+Use this finance tracker to log transactions on spreadsheets. Start the finance tracker via the command
+
+```python
+python3 finance_tracker.py
+```
+
+You will be prompted with 4 inputs one after the other:
+
+```python
+BUSINESS: 
+
+DESCRIPTION:
+
+AMOUNT (e.g 100 | 10.00 | 29.35 | -99.21):
+
+DATE (01/01/2001 | 1/1/2001 | Leave blank for today's date):
+```
+
+The first three inputs will not accept null or empty inputs and will continue to prompt you until you provide valid input.
--- finance_tracker.py
+++ finance_tracker.py
@@ -5,28 +5,51 @@ import os
 import typing
 
 now = datetime.today()
-year = now.year
-month = now.month
+currentYear = now.year
+currentMonth = now.month
 
 # Create yearly csv
-yearly_csv = f'./{year}/overview-{year}.csv'
-if not os.path.exists(f"./{year}"):
-    os.mkdir(f"{year}")
-    with open(yearly_csv, "w") as f:
-        writer = csv.writer(f, quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow([year] + [''] + ["Finances"])
-        writer.writerow(["Description"] + ["Amount"] + ["Date"] + ["Reference"] + ["Transaction ID"])
-        print(f)
+def createYearlyCsv(year = currentYear):
+    yearly_csv = f'./{year}/overview-{year}.csv'
+    if not os.path.exists(f"./{year}"):
+        os.mkdir(f"{year}")
+        with open(yearly_csv, "w") as f:
+            writer = csv.writer(f, quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow([year] + [''] + ["Finances"])
+            writer.writerow(["Description"] + ["Amount"] + ["Date"] + ["Reference"] + ["Transaction ID"])
+            print(f)
+    
+    return yearly_csv
 
 
 # Create monthly csv
-monthly_csv = f"./{year}/{month}-{year}.csv"
-if not os.path.exists(monthly_csv):
-    with open(monthly_csv, "w") as f:
-        writer = csv.writer(f, quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow([year] + [month] + ["Finances"])
-        writer.writerow(["Business"] + ["Description"] + ["Amount"] + ["Date"])
-        print(f)
+def createMonthlyCsv(month = currentMonth, year = currentYear):
+    monthly_csv = f"./{year}/{month}-{year}.csv"
+    if not os.path.exists(monthly_csv):
+        with open(monthly_csv, "w") as f:
+            writer = csv.writer(f, quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow([year] + [month] + ["Finances"])
+            writer.writerow(["Business"] + ["Description"] + ["Amount"] + ["Date"])
+            print(f)
+    
+    return monthly_csv
+
+createYearlyCsv()
+createMonthlyCsv()
+
+
+# Update csvs
+def updateCsvs(finance, month = currentMonth, year = currentYear):
+    currentMonthCsv = createMonthlyCsv(month, year)
+    currentYearCsv = createYearlyCsv(year)
+
+    with open(currentMonthCsv, "a") as f_monthly, open(currentYearCsv, "a") as f_yearly:
+        monthly_writer = csv.writer(f_monthly, quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        yearly_writer = csv.writer(f_yearly, quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+        monthly_writer.writerow([finance.getBusiness()] + [finance.getDescription()] + [finance.getAmount()] + [finance.getDate()])
+        yearly_writer.writerow([finance.getBusiness()] + [finance.getDescription()] + [finance.getAmount()] + [finance.getDate()])
+
 
 # Update sheets
 
@@ -36,6 +59,23 @@ class Finance:
         self.description = ""
         self.amount = float(0)
         self.date = datetime.now()
+
+    def getBusiness(self):
+        return self.business
+
+    def getDescription(self):
+        return self.description
+
+    def getAmount(self):
+        return '{0:.2f}'.format(self.amount)
+    
+    def getDate(self):
+        return self.date
+
+    def getSplitDate(self):
+        # formatted by DAY - MONTH - YEAR
+        # splitDate = self.date.split('/')
+        return self.date.split('/')
     
     def setBusiness(self, business):
         assert isinstance(business, str), f"Business variable must be a string, got {type(business).__name__}"
@@ -58,7 +98,7 @@ class Finance:
             f"----- Finance Object -----\n"
             f"{'Business:':<12} {self.business}\n"
             f"{'Description:':<12} {self.description}\n"
-            f"{'Amount:':<12} ${self.amount:.2f}\n"
+            f"{'Amount:':<12} {'-' if self.amount < 0 else ''}${abs(self.amount):.2f}\n"
             f"{'Date:':<12} {self.date}"
         )
 
@@ -69,30 +109,44 @@ variable_name = {
     3: "DATE (01/01/2001 | 1/1/2001 | Leave blank for today's date): ",
 }
 
+class Steps:
+    BUSINESS = 0
+    DESCRIPTION = 1
+    AMOUNT = 2
+    DATE = 3
+
 while True:
     try:
         finance = Finance()
         for step in range(4):
             variable = input(f"ENTER {variable_name[step]}")
-            while variable is None or len(variable) <= 0:
-                variable = input(f"ENTER {variable_name[step]}")
 
             match step:
-                case 0:
-                    finance.setBusiness(variable)
-                case 1:
-                    finance.setDescription(variable)
-                case 2:
-                    finance.setAmount(round(float(variable), 2))
-                case 3:
-                    finance.setDate((datetime.strptime(variable, "%d/%m/%Y") if variable else datetime.now()).strftime("%d/%m/%Y"))
+                case Steps.BUSINESS:
+                    finance.setBusiness(variable if variable else "NOT PROVIDED")
+                case Steps.DESCRIPTION:
+                    finance.setDescription(variable if variable else "NO DESCRIPTION PROVIDED")
+                case Steps.AMOUNT:
+                    finance.setAmount(round(float(variable), 2) if variable else float(0))
+                case Steps.DATE:
+                    date = variable
+                    splitInput = variable.split("/")
+                    if variable is None or len(variable) <= 0:
+                        date = datetime.now()
 
+                    elif len(splitInput) == 2 or len(splitInput[2]) <= 0:
+                        date = datetime.strptime(f'{splitInput[0]}/{splitInput[1]}/{currentYear}', "%d/%m/%Y")
+
+                    finance.setDate(date.strftime("%d/%m/%Y"))
+
+        financeDate = finance.getSplitDate()
         finance.print()
+        
+        updateCsvs(finance, financeDate[1], financeDate[2])
+
+
     except EOFError:
         print("Exiting...")
         break
     
-# with open(monthly_csv, "a") as f_monthly, open(yearly_csv, "a") as f_yearly:
-#     monthly_writer = csv.writer(f_monthly, quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#     yearly_writer = csv.writer(f_yearly, quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
